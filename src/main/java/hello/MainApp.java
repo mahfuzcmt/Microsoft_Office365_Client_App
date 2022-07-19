973c06ad5da9467244fdd667197f2b23b7318fe9
Mahfuz Ahmed <mahfuz.a@bitmascot.com>
2022-07-19 21:06:26 +0600

    Read emails

--- src/main/java/hello/MainApp.java
+++ src/main/java/hello/MainApp.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -47,7 +48,8 @@ public class MainApp {
         }
         System.out.println("Click on the link to get authorize from Microsoft >> " + url);
         try {
-            getTokenByAuthCode();
+           //getTokenByAuthCode();
+            readEmails();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -70,42 +72,6 @@ public class MainApp {
         return answer.toString();
     }
 
-    public static String doGetRequest(String server) throws IOException {
-        URL url = new URL(server);
-        URLConnection conn = url.openConnection();
-        //conn.setRequestProperty(it.key, it.value);
-        conn.setDoInput(true);
-        conn.setUseCaches(false);
-        return getResponseText(conn);
-    }
-
-    public static URLConnection getPostConnection(String server, String data) throws IOException {
-        URL url = new URL(server);
-        URLConnection conn = url.openConnection();
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-        conn.setUseCaches(false);
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-        writer.write(data);
-        writer.flush();
-        writer.close();
-        return conn;
-    }
-
-    public static String doPostRequest(String server, String data) throws IOException {
-        String responseText = "", errorMessage = "";
-        Throwable throwable;
-        try {
-            URLConnection connection = getPostConnection(server, data);
-            responseText = getResponseText(connection);
-        } catch (Throwable t) {
-
-        }
-        return responseText;
-    }
-
-
     public static void getTokenByAuthCode() throws IOException {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter the code when you will get back from Microsoft:");
@@ -122,10 +88,23 @@ public class MainApp {
         post += "&client_secret="+"FaJhEg1FeW2apA4~9GhGw00-_8f1eKx_-t";
 
         String requestURL = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-        String res = doPostRequest(requestURL, post);
-        System.out.print(res);
 
-        Map<String,Object> result = new ObjectMapper().readValue(res, HashMap.class);
+        URL url = new URL(requestURL);
+        URLConnection conn = url.openConnection();
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setUseCaches(false);
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+        writer.write(post);
+        writer.flush();
+        writer.close();
+        String responseText = getResponseText(conn);
+
+
+        System.out.print(responseText);
+
+        Map<String,Object> result = new ObjectMapper().readValue(responseText, HashMap.class);
 
         System.out.println("Res: "+result);
 
@@ -137,16 +116,41 @@ public class MainApp {
     }
 
 
-    public static void getRefreshToken(String refreshToken){
+    public static void refreshToken(String refreshToken){
         System.out.println("Current refreshToken is : "+refreshToken);
         //TODO need to call API to get refreshed token
         System.out.println("Token successfully refreshed and updated local DB");
     }
 
-    public static void readEmails(String refreshToken){
+    public static void readEmails() throws IOException {
         System.out.println("System going to read user mail : ");
-        //TODO need to call API to get refreshed token
-        System.out.println("Processing mail data to save in database");
+        String folderName = "inbox";
+
+        //Use the token here
+        String access_token = "EwBQA+l3BAAUnQP8Jfa2FYxR0AX7HsEZwOdWa28AAd6FLXu/o9YFRzbTxYrFrieGPVYduCl0rL7GQMPcTu4GNMTwz3KQqLRCP0r/bn/Fn5XtpcNTirxz/6I0phJ77jiHM9Z3R38k9Sam3ti9F6K2u41lq6tJRxxMgEHFeldOlXMwkQDRGKRmsS5mzA3ULe9dNH8AXQ9RKmCka1C96TZeXqJvXx7ARRd19JPCweadpTi67U7+Lbb4JAmOFvl2S9U+X3zgLdCgFzm2RVOfR6KeCP50OVwYls0w3YaEGCqyKzLN7xXHWeUtTRcm9SMROQTO05dUIRjZdwh5hXZaaWh7JsLAXNitVhTjGhg1ghqPjM761l7gPlyF6U+bzin/W7kDZgAACOhjeEvhyymtIAK984ofkzEHtMFFLCHw9waQNyKcVuYG81T6dGc+L36qBqoxl/cSjAfru5xJUmRU2k6AdDaznm/GcbAvCF2Iff084hDyPJtJB/HbMTupCjXRHNWYHMm9dEL5rSnqUnjvPcXw8AbewLJhQsydduFMH/KiJKNwnFus4i2RjOYNQMoRm8KYGP/lz/9AloXIo8U73yl/IxsHJ86DQIUPhF5oCWoqYMRXjrdxOaqDp256FF+KHuVrZm5R8/oWnbaHEBkN3W+nLUWkPKwa/yh3dmOl5I5B4QHUdjM0govB2ai+24LKMbA//OrmnZiWZpkL96coq56jpFNSsp6DOtjvmYsNXMWRj5Re0Q4t1bSR80nKwEW8ksWPM58nSU7WNeggChMoHNv5N3Bd0qFGAJJ7fqxaLI3VVpf81vakdC+bfypUbdxpRyBpwOyblAoXcXdSQgTxdWdElqg8rcJD82eOu0nSOdoIb+bAfxwOy6SUOL1FjmFL9ubCGjiskhXkwadhszUOXDDm2WdmmvHcAz14ZED4hU5aS/5KA6xEu0sHqFFifoeplAXziImHU3GYONrTd5vxQlIGPc6QJmPJ4CHbSY9xlBIxJVjZqYjl3nP+xMPzkpPLHo6WbFAqySPXQv2clIPvRtyPnLhQ4uNRx7iZhCcaqqF3vF4dEhjrsUIQZ5UW38NvgpM+UNedRj/b2xk5LJLSakZGWNCchDdhKIa0f17ed1xpSgI=";
+
+        String mailReadEndpoint = "https://outlook.office.com/api/v2.0/me/MailFolders/"+folderName+"/messages";
+
+        URL url = new URL(mailReadEndpoint);
+        URLConnection conn = url.openConnection();
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Authorization", "Bearer "+access_token);
+        conn.setDoInput(true);
+        conn.setUseCaches(false);
+
+        String responseText = getResponseText(conn);
+        System.out.print(responseText);
+
+        Map<String,Object> result = new ObjectMapper().readValue(responseText, HashMap.class);
+        ArrayList<LinkedHashMap> emails = (ArrayList<LinkedHashMap>) result.get("value");
+
+        for(LinkedHashMap content: emails){
+            LinkedHashMap body = (LinkedHashMap) content.get("Body");
+            System.out.println("BodyPreview: "+content.get("BodyPreview"));
+            System.out.println("Body"+body.get("Content"));
+        }
+
         System.out.println("Done!");
     }
 
